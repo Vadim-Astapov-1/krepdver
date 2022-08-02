b06952f3adcb3aaef5b8ef65c903343282f1ed76
Vadim Astapov <mr.chibichan@yandex.ru>
2022-08-03 00:36:40 +0300

    fear: card for complete-set

--- src/components/CompleteSetCard/CompleteSetCard.js
+++ src/components/CompleteSetCard/CompleteSetCard.js
@@ -1,9 +1,40 @@
-import './CompleteSetCard';
+import { useState } from 'react';
+import './CompleteSetCard.css';
 
-function CompleteSetCard({}) {
+function CompleteSetCard({name, price, description, img}) {
+  const [count, setCount] = useState(1);
+
+  function handleCountClick(evt) {
+    if(evt.target.id === 'complete-set-btn-up') {
+      console.log('more')
+      return setCount(count + 1);
+    }
+
+    if(count === 1) {
+      return;
+    }
+
+    setCount(count - 1);
+  }
+
   return(
     <article className='complete-set__card'>
-
+      <img className='complete-set__img' src={img} alt={name}></img>
+      <h3 className='complete-set__title'>{name}</h3>
+      <div className='complete-set__info'>
+        <p className='complete-set__price'>{`Цена: ${price} руб`}</p>
+        <button className='complete-set__btn-info'>Подробнее</button>
+      </div>
+      <p className='complete-set__description'>{description}</p>
+      <div className='complete-set__btn-container'>
+        <div className='complete-set__calculator'>
+          <button className='complete-set__btn-up' id='complete-set-btn-up' onClick={handleCountClick}></button>
+          <p className='complete-set__count'>{count}</p>
+          <p className='complete-set__count-text'>шт</p>
+          <button className='complete-set__btn-under' id='complete-set-btn-under' onClick={handleCountClick}></button>
+        </div>
+        <button className='complete-set__btn'>Добавить в корзину</button>
+      </div>
     </article>
   );
 }
--- src/components/CompleteSets/CompleteSets.js
+++ src/components/CompleteSets/CompleteSets.js
@@ -1,12 +1,15 @@
 import './CompleteSets.css';
 
+import CompleteSetCard from '../CompleteSetCard/CompleteSetCard';
+import { completeSetsList } from '../../utils/constants';
+
 function CompleteSets() {
   return(
     <section className='complete-sets'>
       <h2 className='complete-sets__title'>Монтажные комплекты для установки дверей</h2>
       <div className='container complete-sets__container'>
         <div className='complete-sets__card-list'>
-          {}
+          {completeSetsList.map((item) => <CompleteSetCard key={item.article} name={item.name} price={item.price} description={item.description} img={item.img} />)}
         </div>
       </div>
     </section>
