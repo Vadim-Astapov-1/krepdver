61efb5a978e5ef407b7c80f34b2d09e4d24214a7
Vadim Astapov <mr.chibichan@yandex.ru>
2022-08-03 18:26:30 +0300

    fear: ExtraCard funk

--- src/components/Extra/Extra.js
+++ src/components/Extra/Extra.js
@@ -3,12 +3,12 @@ import './Extra.css';
 import ExtraCard from '../ExtraCard/ExtraCard';
 import { extraList } from '../../utils/constants';
 
-function Extra() {
+function Extra({ handleCardSubmit }) {
   return (
     <section className='extra'>
       <h2 className='extra__title'>ВАМ МОЖЕТ ПОНАДОБИТЬСЯ</h2>
       <div className='extra__card-list'>
-        {extraList.map((item) => <ExtraCard key={item.article} article={item.article} name={item.name} price={item.price} description={item.description} img={item.img} />)}
+        {extraList.map((item) => <ExtraCard key={item.article} article={item.article} name={item.name} price={item.price} descriptionList={item.descriptionList} img={item.img} />)}
       </div>
     </section>
   );
--- src/components/ExtraCard/ExtraCard.js
+++ src/components/ExtraCard/ExtraCard.js
@@ -1,9 +1,60 @@
 import './ExtraCard.css';
 
-function ExtraCard({}) {
+import CardForm from '../CardForm/CardForm';
+import { useEffect, useState } from 'react';
+
+function ExtraCard({ article, name, descriptionList, price, img, onAddInCart }) {
+  const [count, setCount] = useState(1);
+  const [priceProduct, setPriceProduct] = useState(0);
+
+  function handleCountClick(evt) {
+    if(evt.target.name === 'extra-card-btn-up') {
+      if(count === 99) {
+        return;
+      }
+
+      setCount(count + 1);
+      setPriceProduct(priceProduct + price);
+      return;
+    }
+
+    if(count === 1) {
+      return;
+    }
+
+    setCount(count - 1);
+    setPriceProduct(priceProduct - price);
+  }
+
+  function handleCardSubmit(evt) {
+    evt.preventDefault();
+
+    onAddInCart({
+      name: name,
+      count: count,
+      price: priceProduct,
+    });
+
+    setPriceProduct(price);
+    setCount(1);
+  }
+
+  useEffect(() => {
+    setPriceProduct(price);
+  }, [price]);
+
   return(
     <article className='extra-card'>
-
+      <img className='extra-card__img' src={img} alt={article}></img>
+      <div className='extra-card__info'>
+        <h3 className='extra-card__title'>{article}</h3>
+        <p className='extra-card__subtitle'>{name}</p>
+        {descriptionList.map((item, index) => <p key={index} className='extra-card__paragraph'>{`${index + 1}. ${item}`}</p>)}
+        <div className='extra-card__info-price'>
+          <p className='extra-card__price'>{`Цена: ${priceProduct} руб`}</p>
+          <CardForm onSubmit={handleCardSubmit} onBtnClick={handleCountClick} count={count} place='extra-card' />
+        </div>
+      </div>
     </article>
   );
 }
