c8f9f693d97aad1fa24e78ee91b7a4f062c4a089
Vadim Astapov <mr.chibichan@yandex.ru>
2022-08-03 21:06:48 +0300

    fix: class renaming

--- src/components/Extra/Extra.js
+++ src/components/Extra/Extra.js
@@ -5,10 +5,12 @@ import { extraList } from '../../utils/constants';
 
 function Extra({ handleCardSubmit }) {
   return (
-    <section className='extra line-background-block'>
-      <h2 className='extra__title'>ВАМ МОЖЕТ ПОНАДОБИТЬСЯ</h2>
-      <div className='extra__card-list'>
-        {extraList.map((item) => <ExtraCard key={item.article} article={item.article} name={item.name} price={item.price} descriptionList={item.descriptionList} img={item.img} onAddInCart={handleCardSubmit} />)}
+    <section className='extra'>
+      <div className='container extra__container'>
+        <h2 className='extra__title'>ВАМ МОЖЕТ ПОНАДОБИТЬСЯ</h2>
+        <div className='extra__card-list'>
+          {extraList.map((item) => <ExtraCard key={item.article} article={item.article} name={item.name} price={item.price} descriptionList={item.descriptionList} img={item.img} onAddInCart={handleCardSubmit} />)}
+        </div>
       </div>
     </section>
   );
