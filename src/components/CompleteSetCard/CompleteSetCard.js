86f6bdc35ecc069e36894507905712546f7742bc
Vadim Astapov <mr.chibichan@yandex.ru>
2022-08-25 01:02:43 +0300

    fear: navigate to product page after click on img in card-product

--- src/components/CompleteSetCard/CompleteSetCard.js
+++ src/components/CompleteSetCard/CompleteSetCard.js
@@ -1,14 +1,20 @@
 import './CompleteSetCard.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import CardForm from '../CardForm/CardForm';
 import { Pricing } from '../Pricing/Pricing';
 
 function CompleteSetCard({ id, article, price, description, img, onAddInCart }) {
+  const navigate = useNavigate();
+
   const pricing = Pricing({
     type: 'set-card',
     price: price,
   });
+
+  function handleToProductPage() {
+    navigate(`/product/${id}`)
+  }
 
   function handleCardSubmit(evt) {
     evt.preventDefault();
@@ -24,7 +30,7 @@ function CompleteSetCard({ id, article, price, description, img, onAddInCart }) 
 
   return(
     <article className='set-card'>
-      <img className='set-card__img' src={img} alt={article}></img>
+      <img className='set-card__img' src={img} alt={article} onClick={handleToProductPage}></img>
       <h3 className='set-card__title'>{article}</h3>
       <p className='set-card__price'>{`Цена: ${pricing.priceProduct} руб`}</p>
       <Link to={`/product/${id}`} className='set-card__description'>{description}</Link>
--- src/components/ExtraCard/ExtraCard.js
+++ src/components/ExtraCard/ExtraCard.js
@@ -1,14 +1,20 @@
 import './ExtraCard.css';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import CardForm from '../CardForm/CardForm';
 import { Pricing } from '../Pricing/Pricing';
 
 function ExtraCard({ id, article, name, price, descriptionList, img, onAddInCart }) {
+  const navigate = useNavigate();
+
   const pricing = Pricing({
     type: 'extra-card',
     price: price,
   });
+
+  function handleToProductPage() {
+    navigate(`/product/${id}`)
+  }
 
   function handleCardSubmit(evt) {
     evt.preventDefault();
@@ -24,7 +30,7 @@ function ExtraCard({ id, article, name, price, descriptionList, img, onAddInCart
 
   return(
     <article className='extra-card'>
-      <img className='extra-card__img' src={img} alt={article}></img>
+      <img className='extra-card__img' src={img} alt={article} onClick={handleToProductPage}></img>
       <div className='extra-card__info'>
         <h3 className='extra-card__title'>{article}</h3>
         <Link to={`/product/${id}`} className='extra-card__container-link'>
