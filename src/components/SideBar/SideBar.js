95dcc7182fe790f617d590322c5c4ebd5fc8cad0
Vadim Astapov <mr.chibichan@yandex.ru>
2022-08-24 23:03:08 +0300

    fix: onclick funk on link header nav

--- src/components/SideBar/SideBar.js
+++ src/components/SideBar/SideBar.js
@@ -7,8 +7,8 @@ function SideBar({ isHidden, handleNavMenuVisible }) {
   return(
     <div className={`sidebar ${!isHidden ? 'sidebar_visible' : ''}`}>
       <nav className='sidebar__menu'>
-        <Link to='/' className={`sidebar__link ${location.pathname === '/' ? 'sidebar__link_active' : ''}`}>Главная</Link>
-        <Link to='/cart' className={`sidebar__link ${location.pathname === '/cart' ? 'sidebar__link_active' : ''}`}>Корзина</Link>
+        <Link to='/' className={`sidebar__link ${location.pathname === '/' ? 'sidebar__link_active' : ''}`} onClick={handleNavMenuVisible}>Главная</Link>
+        <Link to='/cart' className={`sidebar__link ${location.pathname === '/cart' ? 'sidebar__link_active' : ''}`} onClick={handleNavMenuVisible}>Корзина</Link>
         <Link to='/' className='sidebar__link' state={{scrollMain: 'complete-sets'}} onClick={handleNavMenuVisible}>Каталог</Link>
         <Link to='/' className='sidebar__link' state={{scrollMain: 'video'}} onClick={handleNavMenuVisible}>Видео</Link>
         <Link to='/' className='sidebar__link' state={{scrollMain: 'map'}} onClick={handleNavMenuVisible}>Карта</Link>
