7bc128161a73dd3443d4e8ebacc808adf7cbaca8
Vadim Astapov <mr.chibichan@yandex.ru>
2022-08-30 15:06:16 +0300

    fear: navigation adaptive design

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,7 +5,7 @@ function Navigation() {
   return(
     <div className='navigation'>
       <nav className='navigation__container'>
-        <Link to='/' className='navigation__link navigation__link_place_first'>Главная</Link>
+        <Link to='/' className='navigation__link'>Главная</Link>
         <Link to='/cart' className='navigation__link'>Корзина</Link>
         <Link to='/' className='navigation__link' state={{scrollMain: 'complete-sets'}}>Каталог</Link>
         <Link to='/' className='navigation__link' state={{scrollMain: 'video'}}>Видео</Link>
