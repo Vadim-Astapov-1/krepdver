5407ab88e649f61424d887a374afd8b2509ffc1e
Vadim Astapov <mr.chibichan@yandex.ru>
2022-08-31 15:50:30 +0300

    fear: product adaptive design full

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -29,43 +29,68 @@ function Product({ article, name, price, description, img, onAddInCart, shortInf
   }, [name]);
 
 
-  return(
+  return (
     <>
-    <section className='product'>
-      <Navigation />
-      <h2 className='product__title'>{name}</h2>
-      <div className='container product__container'>
-        <div className='product__heading'>
-          <img className='product__img' src={img} alt={name}></img>
-          <p className='product__description'>{description}</p>
-          <div className='product__container-price'>
-            <p className='product__price'>{`Цена: ${pricing.priceProduct} руб`}</p>
-            <CardForm onSubmit={handleCardSubmit} onBtnClick={pricing.handleCountClick} count={pricing.count} type='product' />
+      <section className='product'>
+        <Navigation />
+        <h2 className='product__title'>{name}</h2>
+        <div className='container product__container'>
+          <div className='product__heading'>
+            <img className='product__img' src={img} alt={name}></img>
+            <p className='product__description'>{description}</p>
+            {window.innerWidth > 670 ? (
+              <div className='product__container-price'>
+                <p className='product__price'>{`Цена: ${pricing.priceProduct} руб`}</p>
+                <CardForm
+                  onSubmit={handleCardSubmit}
+                  onBtnClick={pricing.handleCountClick}
+                  count={pricing.count}
+                  type='product'
+                />
+              </div>
+            ) : null}
+          </div>
+          <div className='product__property'>
+            <h3 className='product__subtitle'>Описание</h3>
+            <p className='product__text product__text_color_gray'>{`Тип: ${shortInfo.type}`}</p>
+            <p className='product__text product__text_color_gray'>{`Артикул: ${article}`}</p>
+            <p className='product__text product__text_color_gray'>{`Размер: ${shortInfo.size}`}</p>
+          </div>
+          {notice !== null && (
+            <div className='product__property'>
+              <h3 className='product__subtitle'>Особенность</h3>
+              <p className='product__text'>{notice}</p>
+            </div>
+          )}
+        </div>
+        <div className='product__concept'>
+          <div className='product__container-concept'>
+            <ul className='product__capability-list'>
+              {propertyList.map((item, index) => (
+                <li key={index} className='product__capability-item'>
+                  {item}
+                </li>
+              ))}
+            </ul>
+            <h3 className='product__subtitle'>В упаковке:</h3>
+            <p className='product__text product__text_type_package'>
+              {packageInfo}
+            </p>
           </div>
         </div>
-        <div className='product__property'>
-          <h3 className='product__subtitle'>Описание</h3>
-          <p className='product__text product__text_color_gray'>{`Тип: ${shortInfo.type}`}</p>
-          <p className='product__text product__text_color_gray'>{`Артикул: ${article}`}</p>
-          <p className='product__text product__text_color_gray'>{`Размер: ${shortInfo.size}`}</p>
-        </div>
-        {notice !== null &&
-        <div className='product__property'>
-          <h3 className='product__subtitle'>Особенность</h3>
-          <p className='product__text'>{notice}</p>
-        </div>}
-      </div>
-      <div className='product__concept'>
-        <div className='product__container-concept'>
-          <ul className='product__capability-list'>
-            {propertyList.map((item, index) => <li key={index} className='product__capability-item'>{item}</li>)}
-          </ul>
-          <h3 className='product__subtitle'>В упаковке:</h3>
-          <p className='product__text product__text_type_alone'>{packageInfo}</p>
-        </div>
-      </div>
-      <SliderProducts />
-    </section>
+        {window.innerWidth <= 670 ? (
+              <div className='product__container-price'>
+                <p className='product__price'>{`Цена: ${pricing.priceProduct} руб`}</p>
+                <CardForm
+                  onSubmit={handleCardSubmit}
+                  onBtnClick={pricing.handleCountClick}
+                  count={pricing.count}
+                  type='product'
+                />
+              </div>
+            ) : null}
+        <SliderProducts />
+      </section>
     </>
   );
 }
