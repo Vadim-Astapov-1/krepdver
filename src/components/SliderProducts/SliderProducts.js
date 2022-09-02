e1f920d2d5955d952548fd3db5fd96cd22f95b4c
Vadim Astapov <mr.chibichan@yandex.ru>
2022-09-02 17:48:40 +0300

    fear: adaptive slider + slider cards

--- src/components/SliderProducts/SliderProducts.js
+++ src/components/SliderProducts/SliderProducts.js
@@ -1,20 +1,27 @@
 import './SliderProducts.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import SliderProductsCard from '../SliderProductsCard/SliderProductsCard';
 
 function SliderProducts({ productList }) {
   const [translateCoordinates, setTranslateCoordinates] = useState(null);
+  const [step, setStep] = useState(null);
 
   // назначить шаг!
 
   function scrollSliderLeft() {
-
+    console.log(window.innerWidth)
   }
 
   function scrollSliderRight() {
 
   }
+
+  useEffect(() => {
+    if(window.innerWidth > 425) {
+      setStep(200);
+    }
+  }, []);
 
   return(
     <div className='slider-products'>
@@ -22,8 +29,10 @@ function SliderProducts({ productList }) {
       <div className='slider-products__container'>
         <button type='button' className='slider-products__btn' onClick={scrollSliderLeft}></button>
         <button type='button' className='slider-products__btn slider-products__btn_type_right' onClick={scrollSliderRight}></button>
-        <div className='slider-products__list-card'>
-          {productList.map((item) => <SliderProductsCard id={item.id} img={item.img} name={item.name} description={item.description} price={item.price} />)}
+        <div className='slider-products__container-list'>
+          <div className='slider-products__list-card'>
+            {productList.map((item) => <SliderProductsCard key={item.id} id={item.id} img={item.img} name={item.name} description={item.description} price={item.price} />)}
+          </div>
         </div>
       </div>
     </div>
