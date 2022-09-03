3e9d80342bcf417c20b27563af5ca3a69cdb9775
Vadim Astapov <mr.chibichan@yandex.ru>
2022-09-03 14:10:12 +0300

    fear: basis funk of slaider

--- src/components/SliderProducts/SliderProducts.js
+++ src/components/SliderProducts/SliderProducts.js
@@ -1,36 +1,75 @@
 import './SliderProducts.css';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 
 import SliderProductsCard from '../SliderProductsCard/SliderProductsCard';
 
 function SliderProducts({ productList }) {
-  const [translateCoordinates, setTranslateCoordinates] = useState(null);
+  const [translateX, setTranslateX] = useState(0);
   const [step, setStep] = useState(null);
+  const listCardRef = useRef();
 
-  // назначить шаг!
+  const location = useLocation();
 
-  function scrollSliderLeft() {
-    console.log(window.innerWidth)
+  function handleLotSteps( widthCard, twoSteps) {
+    if(twoSteps) {
+      return setStep(widthCard * 2);
+    }
+
+    setStep(widthCard);
   }
 
-  function scrollSliderRight() {
+  function  calculateStep({ pcCardWidht, laptopCardWidht, ipadCardWidht }) {
+    // Если не делится без остатка, то будет прокручиваться одна карточка, иначе две.
+    const twoSteps = productList % 2 === 0;
+    const widthShortCard = listCardRef.current.offsetWidth;
 
+    if(window.innerWidth > 1024) {
+      return handleLotSteps(pcCardWidht, twoSteps);
+    }
+
+    if(window.innerWidth > 768) {
+      return handleLotSteps(laptopCardWidht, twoSteps);
+    }
+
+    if(window.innerWidth > 425) {
+      return handleLotSteps(ipadCardWidht, twoSteps);
+    }
+
+    if(window.innerWidth <= 425) {
+      console.log(widthShortCard)
+      return setStep(widthShortCard);
+    }
+  }
+
+  function scrollLeft() {
+    setTranslateX(translateX + step);
+  }
+
+  function scrollRight() {
+    setTranslateX(translateX - step);
   }
 
   useEffect(() => {
-    if(window.innerWidth > 425) {
-      setStep(200);
-    }
-  }, []);
+    calculateStep({
+      pcCardWidht: 246,
+      laptopCardWidht: 236,
+      ipadCardWidht: 232,
+    });
+  }, [calculateStep]);
+
+  useEffect(() => {
+    setTranslateX(0);
+  }, [location.pathname]);
 
   return(
     <div className='slider-products'>
       <h2 className='slider-products__title'>Рекомендуемые товары</h2>
       <div className='slider-products__container'>
-        <button type='button' className='slider-products__btn' onClick={scrollSliderLeft}></button>
-        <button type='button' className='slider-products__btn slider-products__btn_type_right' onClick={scrollSliderRight}></button>
-        <div className='slider-products__container-list'>
-          <div className='slider-products__list-card'>
+        <button type='button' className='slider-products__btn' onClick={scrollLeft}></button>
+        <button type='button' className='slider-products__btn slider-products__btn_type_right' onClick={scrollRight}></button>
+        <div className='slider-products__container-list' ref={listCardRef}>
+          <div className='slider-products__list-card' style={{transform: `translateX(${translateX}px)`}}>
             {productList.map((item) => <SliderProductsCard key={item.id} id={item.id} img={item.img} name={item.name} description={item.description} price={item.price} />)}
           </div>
         </div>
