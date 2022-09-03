6a51ff58da502cabd72406db26a4693a35fec9ff
Vadim Astapov <mr.chibichan@yandex.ru>
2022-09-03 16:56:27 +0300

    fix: full functional

--- src/components/SliderProducts/SliderProducts.js
+++ src/components/SliderProducts/SliderProducts.js
@@ -7,9 +7,18 @@ import SliderProductsCard from '../SliderProductsCard/SliderProductsCard';
 function SliderProducts({ productList }) {
   const [translateX, setTranslateX] = useState(0);
   const [step, setStep] = useState(null);
+  const [position, setPosition] = useState(0);
+  const [lastPosition, setLastPosition] = useState(0);
+
   const listCardRef = useRef();
 
   const location = useLocation();
+
+  const handleLastPostion = useCallback((widthCard) => {
+    const takedPosition = Math.round(listCardRef.current.offsetWidth / widthCard)
+
+    setLastPosition(productList.length - takedPosition);
+  }, [productList]);
 
   function handleLotSteps( widthCard, twoSteps) {
     if(twoSteps) {
@@ -21,30 +30,39 @@ function SliderProducts({ productList }) {
 
   const calculateStep = useCallback(({ pcCardWidht, laptopCardWidht, ipadCardWidht }) => {
     const twoSteps = productList % 2 === 0;
-    const widthShortCard = listCardRef.current.offsetWidth;
 
     if(window.innerWidth > 1024) {
-      return handleLotSteps(pcCardWidht, twoSteps);
+      handleLastPostion(pcCardWidht)
+      handleLotSteps(pcCardWidht, twoSteps);
+      return
     }
 
     if(window.innerWidth > 768) {
-      return handleLotSteps(laptopCardWidht, twoSteps);
+      handleLastPostion(laptopCardWidht)
+      handleLotSteps(laptopCardWidht, twoSteps);
+      return
     }
 
     if(window.innerWidth > 425) {
-      return handleLotSteps(ipadCardWidht, twoSteps);
+      handleLastPostion(ipadCardWidht)
+      handleLotSteps(ipadCardWidht, twoSteps);
+      return
     }
 
     if(window.innerWidth <= 425) {
-      return setStep(widthShortCard);
+      setLastPosition(productList.length - 1);
+      setStep(listCardRef.current.offsetWidth);
+      return
     }
-  }, [productList])
+  }, [productList, handleLastPostion]);
 
   function scrollLeft() {
+    setPosition(position - 1);
     setTranslateX(translateX + step);
   }
 
   function scrollRight() {
+    setPosition(position + 1);
     setTranslateX(translateX - step);
   }
 
@@ -57,6 +75,7 @@ function SliderProducts({ productList }) {
   }, [calculateStep]);
 
   useEffect(() => {
+    setPosition(0);
     setTranslateX(0);
   }, [location.pathname]);
 
@@ -64,8 +83,8 @@ function SliderProducts({ productList }) {
     <div className='slider-products'>
       <h2 className='slider-products__title'>Рекомендуемые товары</h2>
       <div className='slider-products__container'>
-        <button type='button' className='slider-products__btn' onClick={scrollLeft}></button>
-        <button type='button' className='slider-products__btn slider-products__btn_type_right' onClick={scrollRight}></button>
+        {position > 0 && <button type='button' className='slider-products__btn' onClick={scrollLeft}></button>}
+        {position < lastPosition && <button type='button' className='slider-products__btn slider-products__btn_type_right' onClick={scrollRight}></button>}
         <div className='slider-products__container-list' ref={listCardRef}>
           <div className='slider-products__list-card' style={{transform: `translateX(${translateX}px)`}}>
             {productList.map((item) => <SliderProductsCard key={item.id} id={item.id} img={item.img} name={item.name} description={item.description} price={item.price} />)}
