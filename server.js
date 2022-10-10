bd5a87b698ecf809fc89a3902f8db2ebfd812d12
Vadim Astapov <mr.chibichan@yandex.ru>
2022-10-10 17:50:20 +0300

    feat: yandex menrika + test node server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(__dirname));
+app.use(express.static(path.resolve(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+})
+
+app.listen(3000);
