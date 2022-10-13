4ea93de737152702230c39a384c5562896fee5f0
Vadim Astapov <mr.chibichan@yandex.ru>
2022-10-14 01:18:06 +0300

    fix: serve try 4

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 })
 
 app.listen(3000);
