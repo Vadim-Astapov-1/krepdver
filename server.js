80df6d0a18b0d9888767b32844c1e660aa24e657
Vadim Astapov <mr.chibichan@yandex.ru>
2022-10-13 20:37:04 +0300

    fix: server

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, 'dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-})
-
-app.listen(3000);
+app.listen(PORT);
