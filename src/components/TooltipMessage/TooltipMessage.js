9e01ea487b4d4b1cbd2665ea873ea00ee486c943
Vadim Astapov <mr.chibichan@yandex.ru>
2022-10-18 02:14:49 +0300

    fix: message blocking background content

--- src/components/TooltipMessage/TooltipMessage.js
+++ src/components/TooltipMessage/TooltipMessage.js
@@ -2,7 +2,7 @@ import './TooltipMessage.css';
 
 function TooltipMessage({ isAddInCart }) {
   return (
-    <div className='tooltip-message'>
+    <div className={`tooltip-message ${isAddInCart ? 'tooltip-message_active' : ''}`}>
       <p className={`tooltip-message__text ${isAddInCart ? 'tooltip-message__text_active' : ''}`}>
         Товар добавлен в корзину
       </p>
